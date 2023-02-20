ca5e49c171fdba24ddcc255a15fb156519d37a53
Akbarshokh <bumur@DESKTOP>
2023-02-20 22:53:54 +0200

    [simple change]

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className="header__socials">
     <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-    <a href="https://github" target="_blank"><ImGithub/></a>
+    <a href="https://github.com/Akbarshokh13" target="_blank"><ImGithub/></a>
     <a href="https://instagram.com" target="_blank"><BsInstagram/></a> 
     </div>
   ) 
