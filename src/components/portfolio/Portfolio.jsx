c753c5f9568cbc7c2813c7c07fea1d208cf8447d
Akbarshokh <bumur@DESKTOP>
2023-03-04 13:20:56 +0200

    added few projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,47 +3,45 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
+import IMG4 from '../../assets/port-web.png'
+import IMG5 from '../../assets/notes-view.png'
 
 const data = [
 {
   id: 1,
   image: IMG1,
-  title: 'Image Changer',
-  github: 'https://github.com/Akbarshokh13/Image-changer',
-  demo: 'https://dribbble.com/shots/20676490-Vitamin-Reminder-Mobile-IOS-App?utm_source=Clipboard_Shot&utm_campaign=purrwebui&utm_content=Vitamin%20Reminder%20Mobile%20IOS%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=purrwebui&utm_content=Vitamin%20Reminder%20Mobile%20IOS%20App&utm_medium=Social_Share' 
- 
+  title: 'Blurry Loading',
+  github: 'https://github.com/Akbarshokh13/blurry-loading',
+
 },
 {
   id: 2,
   image: IMG2,
-  title: 'click move',
+  title: 'Click To Move',
   github: 'https://github.com/Akbarshokh13/move-on-click',
-  demo: 'https://dribbble.com/shots/20681639-Mobile-Streaming-App?utm_source=Clipboard_Shot&utm_campaign=metacarbon&utm_content=Mobile%20Streaming%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=metacarbon&utm_content=Mobile%20Streaming%20App&utm_medium=Social_Share' 
- 
+
 },
 
 {
   id: 3,
   image: IMG3,
-  title: 'Dribble',
-  github: 'https://github.com', 
-  demo: 'https://dribbble.com/shots/20681639-Mobile-Streaming-App?utm_source=Clipboard_Shot&utm_campaign=metacarbon&utm_content=Mobile%20Streaming%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=metacarbon&utm_content=Mobile%20Streaming%20App&utm_medium=Social_Share' 
+  title: 'Image Magnifier',
+  github: 'https://github.com/Akbarshokh13/image-magnifier' 
+
 },
 {
   id: 4,
   image: IMG4,
-  title: 'Move on Click',
-  github: 'https://github.com', 
-  demo: 'https://dribbble.com/shots/20678641-Matter-Course-Dark-Mode-E-Course-Mobile-App-Concept?utm_source=Clipboard_Shot&utm_campaign=ihyaet&utm_content=Matter%20Course%20%5BDark%20Mode%5D%20-%20E-Course%20Mobile%20App%20Concept&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=ihyaet&utm_content=Matter%20Course%20%5BDark%20Mode%5D%20-%20E-Course%20Mobile%20App%20Concept&utm_medium=Social_Share' 
+  title: 'React Portfolio Web',
+  github: 'https://github.com/Akbarshokh13/react-my-portfolio-web', 
+  demo: 'https://akbarshokh-u-pro.netlify.app/'
 },
 {
   id: 5,
   image: IMG5,
-  title: 'Image Changer',
-  github: 'https://github.com',
-  demo: 'https://dribbble.com/shots/20677808-Crypto-Finance-App?utm_source=Clipboard_Shot&utm_campaign=awsmd&utm_content=Crypto%20Finance%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=awsmd&utm_content=Crypto%20Finance%20App&utm_medium=Social_Share' 
+  title: 'React Notes App',
+  github: 'https://github.com/Akbarshokh13/react-notes-app',
+  demo: 'https://react-notes-app-project.netlify.app/'
 }
 
 ]
